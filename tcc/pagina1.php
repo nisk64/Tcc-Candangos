0af3b7e13c4d54d62ce788e76d9e49e7be3cabf0
Gabriel Lamberti <lamberti.gabriel2008@gmail.com>
2024-11-13 22:21:54 -0300

    tcc/pagina_Agenda.ptcc/pagina1.php tcc/pagina_Agenda.php pagina_Pac.php tcc/pagina_Pac.phphp tcc/pagina1.php

--- tcc/pagina1.php
+++ tcc/pagina1.php
@@ -8,10 +8,15 @@
 <body>
     <div class="cabecalho"> <!-- classe para cabeçalho do site -->
         <div>
-            <p>Home <a href="pagina1.php"></a> </p>
-            <p>Pacientes</p>
-            <p>Agenda</p>
+            <p>Home <a href="pagina1.php"></a></p> &nbsp;&nbsp;
+            <p>Pacientes <a href="pagina_Pac.php"></a></p> &nbsp;&nbsp;
+            <p>Agenda <a href="pagina_Pac.php"></a></p> &nbsp;&nbsp;
         </div>
+
+
+        <h3>colocar alguma coisa ai falando da empresa</h3>
+
+
     </div>
 </body>
 </html>
